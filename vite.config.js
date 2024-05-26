6a518ba49471de7669d883bfc47a04a7e0ac7f0f
simba <153499114+simba0808@users.noreply.github.com>
2024-05-27 09:32:22 +1000

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [react()]
 });
